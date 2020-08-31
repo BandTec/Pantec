04db3d27726ee1b20e9da56054918a0fe3eb140f
v848 <vitoria.reis@bandtec.com.br>
2020-08-31 20:25:34 -0300

    adicionei o site

---
+++ projeto-site/routes/usuarios.js
@@ -0,0 +1,148 @@
+var express = require('express');
+var router = express.Router();
+var sequelize = require('../models').sequelize;
+var Usuario = require('../models').Usuario;
+var AlunosAulas = require('../models').AlunosAulas;
+
+let sessoes = [];
+
+/* Recuperar usuário por login e senha */
+router.post('/autenticar', function(req, res, next) {
+	console.log('Recuperando usuário por login e senha');
+
+	var login = req.body.login; // depois de .body, use o nome (name) do campo em seu formulário de login
+	var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login	
+	
+	//modificar o select de acordo com as minhas tabelas
+	let instrucaoSql = `select * from alunos where email='${login}' and senha='${senha}'`;
+	console.log(instrucaoSql);
+
+	sequelize.query(instrucaoSql, {
+		model: Usuario
+	}).then(resultado => {
+		console.log(`Encontrados: ${resultado.length}`);
+
+		if (resultado.length == 1) {
+			sessoes.push(resultado[0].dataValues.email); //mudar login por email pois é isso que estou usando
+			console.log('sessoes: ',sessoes);
+			res.json(resultado[0]);
+		} else if (resultado.length == 0) {
+			res.status(403).send('Login e/ou senha inválido(s)');
+		} else {
+			res.status(403).send('Mais de um usuário com o mesmo login e senha!');
+		}
+
+	}).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+  	});
+});
+
+/* Cadastrar usuário */
+router.post('/cadastrar', function(req, res, next) {
+	console.log('Criando um usuário');
+	
+	Usuario.create({
+		nome : req.body.nome,
+		login : req.body.login,
+		senha: req.body.senha
+	}).then(resultado => {
+		console.log(`Registro criado: ${resultado}`)
+        res.send(resultado);
+    }).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+  	});
+});
+
+
+/* Verificação de usuário */
+router.get('/sessao/:login', function(req, res, next) {
+	let login = req.params.login;
+	console.log(`Verificando se o usuário ${login} tem sessão`);
+	
+	let tem_sessao = false;
+	for (let u=0; u<sessoes.length; u++) {
+		if (sessoes[u] == login) {
+			tem_sessao = true;
+			break;
+		}
+	}
+
+	if (tem_sessao) {
+		let mensagem = `Usuário ${login} possui sessão ativa!`;
+		console.log(mensagem);
+		res.send(mensagem);
+	} else {
+		res.sendStatus(403);
+	}
+	
+});
+
+
+/* Logoff de usuário */
+router.get('/sair/:login', function(req, res, next) {
+	let login = req.params.login;
+	console.log(`Finalizando a sessão do usuário ${login}`);
+	let nova_sessoes = []
+	for (let u=0; u<sessoes.length; u++) {
+		if (sessoes[u] != login) {
+			nova_sessoes.push(sessoes[u]);
+		}
+	}
+	sessoes = nova_sessoes;
+	res.send(`Sessão do usuário ${login} finalizada com sucesso!`);
+});
+
+
+/* Recuperar todos os usuários */
+router.get('/', function(req, res, next) {
+	console.log('Recuperando todos os usuários');
+	Usuario.findAndCountAll().then(resultado => {
+		console.log(`${resultado.count} registros`);
+
+		res.json(resultado.rows);
+	}).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+  	});
+});
+
+//pontos das avaliações
+router.post('/pontuacao/:idUsuario/:idAula/:pontuacao', function(req, res, next) {
+	console.log('Recuperando usuário por login e senha');
+
+	var idUsuario = req.params.idUsuario; // depois de .body, use o nome (name) do campo em seu formulário de login
+	var idAula = req.params.idAula; // depois de .body, use o nome (name) do campo em seu formulário de login	
+	var pontuacao = req.params.pontuacao;
+
+	//modificar o select de acordo com as minhas tabelas
+	let instrucaoSql = `insert into AlunosAulas values (${idUsuario}, ${idAula}, ${pontuacao}, 0);`;
+	console.log(instrucaoSql);
+
+	sequelize.query(instrucaoSql, {
+		model: AlunosAulas
+	}).then(resultado => {
+		console.log(`Encontrados: ${resultado.length}`);
+
+		// mudar isso pelo amor de deus
+		// mudar isso pelo amor de deus
+		// mudar isso pelo amor de deus
+		// mudar isso pelo amor de deus
+		// mudar isso pelo amor de deus
+		// mudar isso pelo amor de deus
+		// mudar isso pelo amor de deus
+		if(resultado.ok){
+			console.log(`deu certo`);
+		}else{
+			console.log(`deu ruim`);
+		}
+
+	}).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+  	});
+});
+
+module.exports = router;
+
